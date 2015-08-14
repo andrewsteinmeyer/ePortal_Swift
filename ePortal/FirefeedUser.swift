ffe609c3c069416b2b47a90d5bd8565bfa1fea04
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2015-08-14 16:59:29 -0400

    adding aws as provider to identify the provider for firebase

---
+++ ePortal/FirefeedUser.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  ePortal
+//
+//  Created by Andrew Steinmeyer on 8/7/15.
+//  Copyright (c) 2015 Andrew Steinmeyer. All rights reserved.
+//
+
+import Cocoa
+
+class User: NSObject {
+
+}
